efb62a08d6ff433565c96eec7e1495af31c136b4
parth.trehan <parth.trehan@qdos.com.au>
2026-06-13 02:38:25 +1000

    Update Hero component: increase paddingTop for improved layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
     <section
       style={{
         backgroundColor: "#0F2B5B",
-        paddingTop: 128,
+        paddingTop: 151,
       }}
     >
       {/* Main content */}
